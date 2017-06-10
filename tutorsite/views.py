4a98845512f2601b1ff54b30f3d048b33264fdff
Sally Bridgewater <sbridgewater24@gmail.com>
2017-06-10 17:13:28 +0100

    Added the 'Who am I? bio section

--- tutorsite/views.py
+++ tutorsite/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'tutorsite/base.html', {})
+    return render(request, 'tutorsite/mainpage.html', {})
