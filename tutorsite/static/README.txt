fa8114a0d2fbaa04a586294750d16fb86df44f1f
Sally Bridgewater <sbridgewater24@gmail.com>
2017-07-01 16:42:54 +0100

    Lots done - should have committed earlier - mainly pricing buttons

---
+++ tutorsite/static/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/JoanezAndrades/pen/GqZVdj.
+
+ Inspired by http://uidsgn.co/inspiration/pricing-table-kd.html
--- tutorsite/static/js/grayscale.js
+++ tutorsite/static/js/grayscale.js
@@ -32,12 +32,16 @@ $('.navbar-collapse ul li a').click(function() {
     $(".navbar-collapse").collapse('hide');
 });
 
+function myhref(web){
+    window.open(web, '_blank');
+}
+
 // Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(53.808110, -1.607156));
 });
 
 function init() {
@@ -48,7 +52,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(53.808110, -1.607156), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
